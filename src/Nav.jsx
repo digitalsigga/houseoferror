33edb1657d0d8c53624a96a09f6eb5397c543019
digitalsigga <sigga.irl@digitalsigga.com>
2024-03-07 15:07:35 +0000

    fix vite.config

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,9 +2,10 @@ function Nav() {
     return (
         <nav>
             <ul>
-                <li><a href="/">Manifesto</a></li>
+                <li><a href="/">About us</a></li>
                 <li><a href="/about">Lookbook</a></li>
                 <li><a href="/contact">Shop</a></li>
+                <li><a href="/contact">Roblox</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
