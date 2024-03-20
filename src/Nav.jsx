bbd9c87d06463e047ab27e14b01effb0d1459a95
digitalsigga <sigga.irl@digitalsigga.com>
2024-03-20 16:05:40 +0000

    fix : now live

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
                 <li><a href="/">About us</a></li>
                 <li><a href="/about">Lookbook</a></li>
                 <li><a href="/contact">Shop</a></li>
-                <li><a href="/contact">Roblox</a></li>
+                {/* <li><a href="/contact">Roblox</a></li> */}
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
